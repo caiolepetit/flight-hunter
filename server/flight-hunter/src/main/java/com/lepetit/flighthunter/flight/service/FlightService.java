a805f1515a04f18f985a4ab225363da50c183b55
Caio Conrad Le Petit Costa <caio.lepetit@b2wdigital.com>
2020-09-13 20:31:35 -0300

    Adicionada Exception que retorna o status code correto

--- server/flight-hunter/src/main/java/com/lepetit/flighthunter/flight/service/FlightService.java
+++ server/flight-hunter/src/main/java/com/lepetit/flighthunter/flight/service/FlightService.java
@@ -6,7 +6,7 @@ import com.lepetit.flighthunter.infrastructure.integrator.SkyPickerIntegrator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Comparator;
 import java.util.List;
@@ -25,7 +25,7 @@ public class FlightService {
         return FlightDTO.builder()
                 .flightDetails(flightDetails.stream()
                                 .min(Comparator.comparing(FlightDetails::getPrice))
-                                .orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND, "Não foi encontrado nenhum voo!")))
+                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encontrado nenhum voo!")))
                 .build();
     }
 }
---
+++ server/flight-hunter/src/test/java/com/lepetit/flighthunter/flight/service/FlightServiceTest.java
@@ -0,0 +1,21 @@
+package com.lepetit.flighthunter.flight.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listFlights() {
+    }
+
+    @Test
+    void getLowestPrice() {
+    }
+}
