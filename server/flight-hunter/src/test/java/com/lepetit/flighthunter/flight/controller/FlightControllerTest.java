f14654cdbd96c5170ec35569f3d9268717c438bb
Caio Conrad Le Petit Costa <caio.lepetit@b2wdigital.com>
2020-09-13 20:32:32 -0300

    Adição de mais testes das classes

--- server/flight-hunter/src/test/java/com/lepetit/flighthunter/flight/controller/FlightControllerTest.java
+++ server/flight-hunter/src/test/java/com/lepetit/flighthunter/flight/controller/FlightControllerTest.java
@@ -1,20 +1,22 @@
 package com.lepetit.flighthunter.flight.controller;
 
+import com.lepetit.flighthunter.flight.domain.FlightDetails;
+import com.lepetit.flighthunter.flight.domain.dto.FlightDTO;
 import com.lepetit.flighthunter.flight.service.FlightService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.NestedServletException;
 
-import java.util.HashMap;
-
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,11 +32,25 @@ public class FlightControllerTest {
 
     @Test
     public void getLowestFlightPrice_whenAirportFromGRUAndToOPOAndCurrencyBRL_thenStatus200 () throws Exception {
+        when(flightService.getLowestPrice("GRU", "OPO", "BRL")).thenReturn(FlightDTO.builder().flightDetails(FlightDetails.builder().cityFrom("São Paulo").build()).build());
+
         mockMvc.perform(get("/flights/lowest-price")
                         .contentType(MediaType.APPLICATION_JSON)
                         .param("airportFrom", "GRU")
                         .param("airportTo", "OPO")
                         .param("currency", "BRL"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getLowestFlightPrice_whenAirportFromIsWrong_thenStatus404 () throws Exception {
+        when(flightService.getLowestPrice("AAA", "OPO", "BRL")).thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encontrado nenhum voo"));
+
+        mockMvc.perform(get("/flights/lowest-price")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("airportFrom", "AAA")
+                .param("airportTo", "OPO")
+                .param("currency", "BRL"))
+                .andExpect(status().isNotFound());
     }
 }
--- server/flight-hunter/src/test/java/com/lepetit/flighthunter/flight/service/FlightServiceTest.java
+++ server/flight-hunter/src/test/java/com/lepetit/flighthunter/flight/service/FlightServiceTest.java
@@ -1,21 +1,101 @@
 package com.lepetit.flighthunter.flight.service;
 
+import com.lepetit.flighthunter.flight.domain.FlightDetails;
+import com.lepetit.flighthunter.flight.domain.Response;
+import com.lepetit.flighthunter.flight.domain.dto.FlightDTO;
+import com.lepetit.flighthunter.infrastructure.integrator.SkyPickerIntegrator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class FlightServiceTest {
+
+    public String airportFrom;
+    public String airportTo;
+    public String currency;
+
+    @Mock
+    private SkyPickerIntegrator skyPickerIntegrator;
+
+    @InjectMocks
+    private FlightService flightService;
 
     @BeforeEach
     void setUp() {
+        this.airportFrom = "GRU";
+        this.airportTo = "OPO";
+        this.currency = "BRL";
     }
 
     @Test
-    void listFlights() {
+    void resultedFlightsSendAllArguments() {
+        List<FlightDetails> flights = new ArrayList<>();
+        flights.add(FlightDetails.builder()
+                .cityFrom("São Paulo")
+                .flyFrom("GRU")
+                .cityTo("Porto")
+                .flyTo("OPO")
+                .build());
+        Response expectedResponse = Response.builder()
+                                    .flightDetails(flights)
+                                    .build();
+
+        when(skyPickerIntegrator.listFlights(this.airportFrom, this.airportTo, this.currency)).thenReturn(expectedResponse);
+
+        List<FlightDetails> resultFlights = flightService.listFlights(this.airportFrom, this.airportTo, this.currency);
+
+        assertEquals(resultFlights, expectedResponse.getFlightDetails());
     }
 
     @Test
-    void getLowestPrice() {
+    void getLowestPriceReturningOk() {
+        List<FlightDetails> flights = new ArrayList<>();
+        flights.add(FlightDetails.builder()
+                .cityFrom("São Paulo")
+                .flyFrom("GRU")
+                .cityTo("Porto")
+                .flyTo("OPO")
+                .price(new BigDecimal(1800))
+                .build());
+        flights.add(FlightDetails.builder()
+                .cityFrom("São Paulo")
+                .flyFrom("GRU")
+                .cityTo("Porto")
+                .flyTo("OPO")
+                .price(new BigDecimal(1000))
+                .build());
+        Response expectedResponse = Response.builder()
+                .flightDetails(flights)
+                .build();
+
+        when(skyPickerIntegrator.listFlights(this.airportFrom, this.airportTo, this.currency)).thenReturn(expectedResponse);
+
+        FlightDTO resultFlight = flightService.getLowestPrice(this.airportFrom, this.airportTo, this.currency);
+
+        assertEquals(resultFlight.getFlightDetails().getPrice(), new BigDecimal(1000));
+    }
+
+    @Test
+    void resultedExceptionWhenSendAirportToWrong() {
+        when(skyPickerIntegrator.listFlights(this.airportFrom, "AAA", this.currency)).thenThrow(new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        Assertions.assertThrows(ResponseStatusException.class, () -> {
+            flightService.getLowestPrice(this.airportFrom, "AAA", this.currency);
+        });
     }
 }
