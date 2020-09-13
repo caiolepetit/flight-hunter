3c1f96a04e17a068dff46902a794fb377e264bf1
Caio Conrad Le Petit Costa <caio.lepetit@b2wdigital.com>
2020-09-13 10:41:55 -0300

    Ajuste no mockBean para funcionamento correto teste

--- server/flight-hunter/src/test/java/com/lepetit/flighthunter/flight/controller/FlightControllerTest.java
+++ server/flight-hunter/src/test/java/com/lepetit/flighthunter/flight/controller/FlightControllerTest.java
@@ -1,9 +1,12 @@
 package com.lepetit.flighthunter.flight.controller;
 
+import com.lepetit.flighthunter.flight.service.FlightService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -22,9 +25,12 @@ public class FlightControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private FlightService flightService;
+
     @Test
     public void getLowestFlightPrice_whenAirportFromGRUAndToOPOAndCurrencyBRL_thenStatus200 () throws Exception {
-        mockMvc.perform(get("/flights")
+        mockMvc.perform(get("/flights/lowest-price")
                         .contentType(MediaType.APPLICATION_JSON)
                         .param("airportFrom", "GRU")
                         .param("airportTo", "OPO")
